0a9715c00f511c70a1ee91587250c15a2ca91610
Swastik Biswas <62232457+PolybitRockzz@users.noreply.github.com>
2024-03-20 09:30:23 +0530

    +1 extension, error handling, minor bug fixes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import time
 from win10toast import ToastNotifier
 
 wait_time = 10 # wait time for the program to run in seconds (10-60 recommended)
-img_ex = [".png", ".jpg", ".jpeg", ".gif"] # image extensions
+img_ex = [".png", ".jpg", ".jpeg", ".gif", ".webp"] # image extensions
 vid_ex = [".mp4", ".avi", ".mov", ".wmv", ".flv", ".webm"] # video extensions
 doc_ex = [".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".pdf"] # document extensions
 music_ex = [".mp3", ".wav", ".aac", ".flac", ".ogg", ".wma"] # music extensions
@@ -38,7 +38,7 @@ while True:
         pass
 
     files = [f for f in os.listdir(downloads) if os.path.isfile(os.path.join(downloads, f))]
-    pics, docs, vids, mus, err = 0, 0, 0, 0, 0
+    pics, docs, vids, mus = 0, 0, 0, 0
 
     for file in files:
         file_name, file_extension = os.path.splitext(os.path.join(downloads, file))
@@ -48,7 +48,14 @@ while True:
                     os.rename(os.path.join(downloads, file), os.path.join(home + "\\Pictures\\Downloads", file))
                     pics += 1
                 except FileExistsError:
-                    err += 1
+                    happens = False
+                    file_attempts = 1
+                    while not happens:
+                        try:
+                            os.rename(os.path.join(downloads, file), os.path.join(home + "\\Pictures\\Downloads", file + f" ({file_attempts})"))
+                            happens = True
+                        except FileExistsError:
+                            file_attempts += 1
                 break
         for x in vid_ex:
             if x == file_extension:
@@ -56,7 +63,14 @@ while True:
                     os.rename(os.path.join(downloads, file), os.path.join(home + "\\Videos\\Downloads", file))
                     vids += 1
                 except FileExistsError:
-                    err += 1
+                    happens = False
+                    file_attempts = 1
+                    while not happens:
+                        try:
+                            os.rename(os.path.join(downloads, file), os.path.join(home + "\\Videos\\Downloads", file + f" ({file_attempts})"))
+                            happens = True
+                        except FileExistsError:
+                            file_attempts += 1
                 break
         for x in doc_ex:
             if x == file_extension:
@@ -64,7 +78,14 @@ while True:
                     os.rename(os.path.join(downloads, file), os.path.join(home + "\\Documents\\Downloads", file))
                     docs += 1
                 except FileExistsError:
-                    err += 1
+                    happens = False
+                    file_attempts = 1
+                    while not happens:
+                        try:
+                            os.rename(os.path.join(downloads, file), os.path.join(home + "\\Documents\\Downloads", file + f" ({file_attempts})"))
+                            happens = True
+                        except FileExistsError:
+                            file_attempts += 1
                 break
         for x in music_ex:
             if x == file_extension:
@@ -72,18 +93,24 @@ while True:
                     os.rename(os.path.join(downloads, file), os.path.join(home + "\\Music\\Downloads", file))
                     mus += 1
                 except FileExistsError:
-                    err += 1
+                    happens = False
+                    file_attempts = 1
+                    while not happens:
+                        try:
+                            os.rename(os.path.join(downloads, file), os.path.join(home + "\\Music\\Downloads", file + f" ({file_attempts})"))
+                            happens = True
+                        except FileExistsError:
+                            file_attempts += 1
                 break
 
-    if (pics + docs + vids + mus + err) > 0:
+    if (pics + docs + vids + mus) > 0:
         toast = ToastNotifier()
         toast.show_toast(f"Total {pics + vids + docs + mus} Downloads Organized ✨",
         "Files organized: \n"
         + ((str(pics) + " pictures\n") if pics > 0 else "")
         + ((str(vids) + " videos\n") if vids > 0 else "")
         + ((str(docs) + " documents\n") if docs > 0 else "")
-        + ((str(mus) + " music\n") if mus > 0 else "")
-        + ((str(err) + " errors\n") if err > 0 else ""), duration=10)
+        + ((str(mus) + " music\n") if mus > 0 else ""))
     
     print(f"Sleeping for {wait_time if wait_time > 0 else 60}s...")
     time.sleep(wait_time if wait_time > 0 else 60)
